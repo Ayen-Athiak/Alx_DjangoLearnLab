770626433c7c5c024630c6b9580d47db31274789
Ayen-Athiak <ayenathiak@gmail.com>
2024-08-18 18:04:55 +0200

    task admin interface

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Book
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_year')
+    search_fields = ('title', 'author')
 
 
-admin.site.register(Book)
+admin.site.register(Book,BookAdmin)
 
