5b0bc7a6df8fc65b393019bf343bbe8519609dfa
Ayen-Athiak <ayenathiak@gmail.com>
2024-08-21 23:27:30 +0200

    tasked

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,11 +1,35 @@
 from relationship_app.models import Author,Book,Librarian,Library
 
+# Query all books by a specific author
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = author.books.all()
+    return books
 
-books = Book.objects.filter(author=author)
-print(books)
+# List all books in a library
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return books
 
-library = Library.objects.get(name=library_name)
-print(library)
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    return librarian
 
-librarian = library.librarian
-print(librarian)
+if __name__ == "__main__":
+    # Example usage
+    print("Books by Author 'J.K. Rowling':")
+    books = get_books_by_author('J.K. Rowling')
+    for book in books:
+        print(book.title)
+
+    print("\nBooks in Library 'Central Library':")
+    books = get_books_in_library('Central Library')
+    for book in books:
+        print(book.title)
+
+    print("\nLibrarian for Library 'Central Library':")
+    librarian = get_librarian_for_library('Central Library')
+    print(librarian.name)
