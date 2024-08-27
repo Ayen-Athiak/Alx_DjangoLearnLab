90c4872685b4fdca9a0f5976d8b377c7f002376b
Ayen-Athiak <ayenathiak@gmail.com>
2024-08-27 12:16:10 +0200

    task views and urls

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,34 @@
+from typing import Any
 from django.shortcuts import render
 
+from django.views.generic import DetailView
+from .models import Book,Librarian,Library,Author
+
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    content = {list_books : books}
+    return render(request, 'list_books.html', content)
+
+
+
+
+class LibraryDetailView(DetailView):
+    
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'  
+    
+    def get_context_data(self, **kwargs):
+        """Add additional context data specific to the library."""
+        context = super().get_context_data(**kwargs)
+        return context
+
+    
+    
+    
+    
+
+
+    
+
