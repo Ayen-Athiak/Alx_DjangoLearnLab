a37dc237b01ffe3e05e78f84019e380daf341e70
Ayen-Athiak <ayenathiak@gmail.com>
2024-08-27 13:21:54 +0200

    booklist

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,12 +8,4 @@ def list_books(request):
 
 
 
-# relationship_app/views.py
-from django.views.generic.detail import DetailView
-from .models import Library
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
 
