817369eedd032f783fa33b9e86b155b808ca2fea
Ayen-Athiak <ayenathiak@gmail.com>
2024-09-01 00:09:12 +0200

    task updated

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,9 +4,9 @@ from django.views.generic import DetailView
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'  # Ensure this template path is correct
+    template_name = 'relationship_app/library_detail.html'  # Ensure this template path is correct
     context_object_name = 'library'
