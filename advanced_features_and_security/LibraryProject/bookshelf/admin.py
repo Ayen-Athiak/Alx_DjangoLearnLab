25cbc8605417da862fcb1a3fc8eda560a8b68e07
Ayen-Athiak <ayenathiak@gmail.com>
2024-09-01 19:43:23 +0200

    task

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,25 +1,48 @@
 from django.contrib import admin
+#from.models import CustomUser
+#from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-from .models import Book
+from.models import Book
 
-from django.contrib import admin
+class admin_book(admin.ModelAdmin):
+    list_display = ('title', 'author' )
+    list_filter = ('title', 'author')
 
-# Register your models here.
+    search_fields = ('title', 'author')
+    fieldsets = (
+        (None, {
+            'classes': ['wide'],
+            'fields': ('username', 'password')
+        }),
+        ('Informations personnelles', {
+            'classes': ['wide'],
+            'fields': ('first_name', 'last_name', 'email', 'avatar')
+        }),
+        ('Permissions', {
+            'classes': ['wide'],
+            'fields': ('is_superuser', 'is_staff', 'is_active', 'groups', 'permissions')
+        }),
+        ('Dates importantes', {
+            'classes': ['wide'],
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
 
-from.models import CustomUser
-from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-# first is the user custom model
+admin.site.register(Book,admin_book)
 
+
+
+
+
+'''
 class CustomUserAdmin(UserAdmin):
     list_display = ('username','email','first_name','last_name','is_staff','is_active')
     search_fields =("email","username","last_name")
   
  
 
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)'''
 
 
 
@@ -30,12 +53,5 @@ admin.site.register(CustomUser, CustomUserAdmin)
 
 
 
-# books class 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    
-    search_fields = ('title', 'author')
 
-
-admin.site.register(Book,BookAdmin)
 
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,45 +1,64 @@
+#from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-
-from django.contrib.auth.models import AbstractUser,BaseUserManager,AbstractBaseUser
-from django.db import models
-
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
 
 
+class Book (models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    
+
+    class Meta:
+        permissions = [
+            ("can_view_book", "Can view book"),
+            ("can_create_book", "Can create book"),
+            ("can_edit_book", "Can edit book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+
+
+
+
+
+''''
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email))
+            raise ValueError(_('The Email field must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
     
-    def create_superuser(self,email,password = None):
-        user = self.create_user(email, password=password)
-        user.is_admin = True
-        user.is_staff = True
-        
-        user.save(using=self._db)
-        return user
-    
+
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True,unique=True, blank=True)
+    email = models.EmailField(_('email address'), unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-
+    objects =CustomUserManager()
+ 
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager()
+
+    
+
+    
 
     def __str__(self):
         return self.email
+'''
 
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+
 
