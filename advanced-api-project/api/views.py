ecd019c0f5d48ca9d8b41f5ac1143d84a548c1b2
Ayen-Athiak <ayenathiak@gmail.com>
2024-09-14 02:41:47 +0200

    task1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,36 +1,14 @@
-from django.shortcuts import render
-from .serializers import BookSerializer,AuthorSerializer
-from rest_framework.permissions import AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissionsOrAnonReadOnly
-from .models import Book,Author
-from rest_framework.generics import (
-    ListAPIView,CreateAPIView,UpdateAPIView,DestroyAPIView,
-)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from .models import Author,Book
+from .serializers import AuthorSerializer,BookSerializer
 
-# Create your views here.
-from rest_framework.permissions import IsAuthenticated, AllowAny
-
- 
-class BookCreateAPIView(generices.CreateAPIView):
+class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]  # Allow unauthenticated users to list books
+    permission_classes = [IsAuthenticated]
 
-class BookUpdateAPIView(generics.UpdateAPIView):
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
-
-class BookDestroyAPIView (generics.DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class BookListAPIView(generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-
-class BookDetailAPIView(generics.DetailAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    
+    permission_classes = [IsAuthenticated]
