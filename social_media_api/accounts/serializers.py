4178b177eaa4872eb47dfac11c5d1da8fd58c175
Ayen-Athiak <ayenathiak@gmail.com>
2024-09-21 11:53:40 +0200

    updated

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,7 +11,6 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'email', 'bio', 'profile_picture')
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    # Include a password field with write-only access
     password = serializers.CharField(write_only=True)
 
     class Meta:
@@ -19,12 +18,18 @@ class RegistrationSerializer(serializers.ModelSerializer):
         fields = ('username', 'email', 'password', 'bio', 'profile_picture')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        # Create a new user and set the password
-        user = User(**validated_data)
-        user.set_password(validated_data['password'])
-        user.save()
+# Create a new user using the built-in create_user method
 
-        # Create a token for the new user
+    def create(self, validated_data):
+      
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None)
+        )
+
+     
         Token.objects.create(user=user)
         return user
